67570bede0ce570f58f0e3078faa80505c58c0a6
Blue-Corn <42467285+dhruvkapadia5786@users.noreply.github.com>
2023-09-25 12:39:36 +0530

    Task manager updated code
    Created a simple todo task with add & delete functionality using tailwind css & angular material

---
+++ src/app/components/task-form/task-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Task } from 'src/app/constants/tasks.interface';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.css']
+})
+export class TaskFormComponent {
+  tasks: Task[] = [];
+  newTask: string = '';
+  constructor() { }
+  ngOnInit(): void {
+  }
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      const newTask: Task = {
+        name: this.newTask,
+        completed: false
+      };
+      this.tasks.push(newTask);
+      this.newTask = '';
+    }
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+    theme: {
+      extend: {
+        colors: {
+          'my-blue': '#000000',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
